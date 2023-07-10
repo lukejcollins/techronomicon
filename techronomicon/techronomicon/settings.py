6d162149a050d8cbce7bbbeee6f975f8276c919e
Luke Collins <luke.collins@live.co.uk>
2023-07-10 21:02:29 +0100

    Add secure proxy header

--- techronomicon/techronomicon/settings.py
+++ techronomicon/techronomicon/settings.py
@@ -164,3 +164,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Set SSL header
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
