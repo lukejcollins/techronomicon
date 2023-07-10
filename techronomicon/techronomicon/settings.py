e8985decc08d60eb4fb70e3e4996d236eb2964c9
Luke Collins <luke.collins@live.co.uk>
2023-07-10 16:31:30 +0100

    Update Allowed Hosts

--- techronomicon/techronomicon/settings.py
+++ techronomicon/techronomicon/settings.py
@@ -61,7 +61,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['34.244.180.26']
 
 
 # Application definition
