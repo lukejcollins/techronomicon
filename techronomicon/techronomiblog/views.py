3abd957cea29fc598f993293d3cc447e331a75f1
Luke Collins <luke.collins@live.co.uk>
2024-02-08 22:42:32 +0000

    Add about section and nav bar

--- techronomicon/techronomiblog/views.py
+++ techronomicon/techronomiblog/views.py
@@ -4,3 +4,6 @@ from .models import Post
 def home(request):
     posts = Post.objects.order_by('-pub_date')
     return render(request, 'techronomiblog/home.html', {'posts': posts})
+
+def about(request):
+    return render(request, 'techronomiblog/about.html')
