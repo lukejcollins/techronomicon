91124562f397084e6c163c43af807bda8cc01a87
Luke Collins <luke.collins@live.co.uk>
2024-02-11 12:49:12 +0000

    Enable markdown support

--- techronomicon/techronomiblog/admin.py
+++ techronomicon/techronomiblog/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
 from .models import Post
+from markdownx.admin import MarkdownxModelAdmin
 
-admin.site.register(Post)
+admin.site.register(Post, MarkdownxModelAdmin)
--- techronomicon/techronomiblog/views.py
+++ techronomicon/techronomiblog/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from .models import Post
+from markdownx.utils import markdownify
 
 def home(request):
     posts = Post.objects.order_by('-pub_date')
+    
+    # Convert Markdown content to HTML for each post
+    for post in posts:
+        post.content = markdownify(post.content)
+    
     return render(request, 'techronomiblog/home.html', {'posts': posts})
 
 def about(request):
     return render(request, 'techronomiblog/about.html')
+
