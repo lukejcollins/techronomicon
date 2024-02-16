2e465bd8648536bc073cea9f1e1f0aefbdd5d77a
Luke Collins <luke.collins@live.co.uk>
2024-02-16 18:48:01 +0000

    Add favicons and a meta description

--- techronomicon/techronomicon/settings.py
+++ techronomicon/techronomicon/settings.py
@@ -156,14 +156,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATICFILES_DIRS = [BASE_DIR / 'techronomiblog/static']
-
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
