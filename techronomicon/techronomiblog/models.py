f50907e68fc43639265b46f56e96319cdae737f1
Luke <44213313+lukejcollins@users.noreply.github.com>
2024-03-12 21:14:28 +0000

    Add fields for maintainin opengraph data (#49)

--- techronomicon/techronomiblog/models.py
+++ techronomicon/techronomiblog/models.py
@@ -5,6 +5,9 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     content = MarkdownxField()  # Use MarkdownxField for Markdown support
     pub_date = models.DateTimeField(auto_now_add=True)
+    og_title = models.CharField(max_length=200, blank=True)
+    og_description = models.TextField(blank=True)
+    og_image = models.ImageField(upload_to='og_images/', blank=True)
     
     def get_truncated_content(self):
         return self.content[:500] + '...' if len(self.content) > 500 else self.content
