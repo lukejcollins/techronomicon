80c94977abf4af6adcdd3a236537365a9528d25a
Luke <44213313+lukejcollins@users.noreply.github.com>
2024-08-05 19:46:01 +0100

    Ignore specific type checks (#60)

--- techronomicon/techronomiblog/admin.py
+++ techronomicon/techronomiblog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Post
-from markdownx.admin import MarkdownxModelAdmin
+from markdownx.admin import MarkdownxModelAdmin  # type: ignore
 from .models import AboutPage
 
 class PostAdmin(MarkdownxModelAdmin):
